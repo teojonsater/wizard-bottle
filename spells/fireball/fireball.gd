1d1e2d83f2c99f7c11a10b2432e85696e37add8f
Teo J <36267770+teojonsater@users.noreply.github.com>
2025-01-19 22:14:03 +0100

    Fireball tas nu bort när den är utanför skärmen

--- spells/fireball/fireball.gd
+++ spells/fireball/fireball.gd
@@ -1,14 +1,17 @@
 extends BaseSpell
 
-# @export var spawn_offset := Vector2(30, 0)
-# @export var speed: float = 250
+@export var spawn_offset := Vector2(30, 0)
+@export var speed: float = 250
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super()
-	# self.position = self.caster.position + self.spawn_offset
+	self.position = self.caster.position + self.spawn_offset
 
 
 func _process(delta: float) -> void:
-	# self.position.x += speed * delta
-	pass
+	self.position.x += speed * delta
+
+
+func _on_screen_exit() -> void:
+	self.queue_free()
